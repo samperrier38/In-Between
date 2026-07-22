20ecf3d99101a0ec3c0507c3a1cb1742234c960f
Sam Perrier <samuelperrier38@gmail.com>
2026-07-22 17:57:07 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const learnButton = document.getElementById("learnButton");
+const message = document.getElementById("message");
+learnButton.addEventListener("click", function () {
+
+    message.textContent =
+        "Connection grows when we choose to understand one another. The space between people doesn't have to divide us—it can become a place where empathy, trust, and meaningful relationships are built.";
+
+});
+learnButton.textContent = "Thanks for Reflecting!";
